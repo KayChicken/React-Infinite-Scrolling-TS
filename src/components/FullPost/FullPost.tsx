06cff6d2bc3375105464f67d262d37659a3fc501
KayChiken <kainov.dmitry370@yandex.ru>
2023-06-17 20:30:32 +0300

    infinity-scroll

--- src/components/FullPost/FullPost.tsx
+++ src/components/FullPost/FullPost.tsx
@@ -30,18 +30,21 @@ const FullPost: React.FC = () => {
     console.log(post)
 
     return (
-        <div className={styles.container}>
+        <div className={styles.container}>  
             {isLoading ? (
-                <div className={styles.skeletonContainer}>
-                    <Skeleton/>
-                </div>) : (
+                
+                    <Skeleton />
+                ) : (
                 <div>
                     <div className={styles.imgContainer}>
                         <img className={styles.img} src={post?.url} alt="" />
                     </div>
                     <div className={styles.content}>
                         <div className={styles.title}>{post?.title}</div>
-                        <div className={styles.body}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed justo tempus, cursus dui quis, posuere leo. Ut sagittis maximus lorem, sit amet venenatis neque interdum id. Cras at justo felis. Donec eu mauris metus. Praesent sit amet semper risus, et convallis lacus. Sed sollicitudin lectus id consectetur pharetra. Pellentesque imperdiet viverra viverra. Mauris rhoncus aliquam velit, vel elementum ex porta ornare. Mauris et cursus risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed posuere, neque sit amet porttitor sodales, est enim congue leo, non vehicula velit leo et odio. Duis blandit ac lectus et imperdiet. Donec vel pulvinar nibh. Nullam sodales nisl orci, et scelerisque lacus fermentum in. Cras tincidunt lectus eu felis gravida finibus.</div>
+                        <div className={styles.bodyContainer}>
+                            <div className={styles.body}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed justo tempus, cursus dui quis, posuere leo. Ut sagittis maximus lorem, sit amet venenatis neque interdum id. Cras at justo felis. Donec eu mauris metus. Praesent sit amet semper risus, et convallis lacus. Sed sollicitudin lectus id consectetur pharetra. Pellentesque imperdiet viverra viverra. Mauris rhoncus aliquam velit, vel elementum ex porta ornare. Mauris et cursus risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed posuere, neque sit amet porttitor sodales, est enim congue leo, non vehicula velit leo et odio. Duis blandit ac lectus et imperdiet. Donec vel pulvinar nibh. Nullam sodales nisl orci, et scelerisque lacus fermentum in. Cras tincidunt lectus eu felis gravida finibus.</div>
+                        </div>
+
 
                     </div>
                 </div>
--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -4,17 +4,16 @@ import ContentLoader from "react-content-loader"
 const Skeleton : React.FC<any> = (props : any) => (
     <ContentLoader 
     speed={2}
-    width={500}
-    height={400}
-    viewBox="0 0 500 400"
+    width={700}
+    height={549}
+    viewBox="0 0 700 549"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
     {...props}
   >
-    <rect x="88" y="96" rx="0" ry="0" width="1" height="53" /> 
-    <rect x="23" y="-6" rx="0" ry="0" width="264" height="237" /> 
-    <rect x="23" y="279" rx="0" ry="0" width="264" height="25" /> 
-    <rect x="23" y="244" rx="0" ry="0" width="264" height="25" />
+    <rect x="0" y="367" rx="0" ry="0" width="600" height="158" /> 
+    <rect x="0" y="313" rx="0" ry="0" width="600" height="42" /> 
+    <rect x="0" y="-1" rx="0" ry="0" width="600" height="300" />
   </ContentLoader>
 )
 
